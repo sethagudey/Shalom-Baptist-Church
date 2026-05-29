c682d552b9556ae6032fc20e7337cb6f99f411aa
Seth A. Agudey <137450706+sethagudey@users.noreply.github.com>
2026-05-29 19:14:43 +0000

    Create CTASection.js

---
+++ src/sections/CTASection.js
@@ -0,0 +1,19 @@
+export default function CTASection() {
+  return (
+    <section className="bg-accent text-center">
+      <div className="container-custom">
+        <h2 className="text-4xl font-bold mb-6">
+          Join The Movement
+        </h2>
+
+        <p className="text-lg mb-8">
+          You belong here.
+        </p>
+
+        <button className="bg-black px-8 py-4 rounded-full">
+          Connect With Us
+        </button>
+      </div>
+    </section>
+  );
+}
