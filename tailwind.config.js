18a74c9e36a99287c92c2f358821f993773c16d3
Seth A. Agudey <137450706+sethagudey@users.noreply.github.com>
2026-05-29 18:59:37 +0000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb",
+        dark: "#020617",
+        accent: "#f97316",
+        light: "#f8fafc"
+      }
+    },
+  },
+  plugins: [],
+}
