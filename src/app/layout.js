9de18057a4641a96065a318628fef9e09fc1ac4c
Seth A. Agudey <137450706+sethagudey@users.noreply.github.com>
2026-05-29 19:04:50 +0000

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,1 +1,14 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Revive Youth Church",
+  description: "Faith. Purpose. Community.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
