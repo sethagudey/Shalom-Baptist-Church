c4dff338658f55c2a028f0931fcf613dc58293c9
Seth A. Agudey <137450706+sethagudey@users.noreply.github.com>
2026-05-29 19:15:26 +0000

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black py-10">
+      <div className="container-custom text-center">
+        <h2 className="text-2xl font-bold mb-4">
+          REVIVE
+        </h2>
+
+        <p className="text-gray-400">
+          Faith. Purpose. Community.
+        </p>
+      </div>
+    </footer>
+  );
+}
